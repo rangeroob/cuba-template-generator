1b521543a0506dd1df257b6bcbdb602b3e88f38b
derekviera <ma.dmviera01@gmail.com>
2017-10-07 00:52:00 -0400

    Add rspec test for gemfile creation

--- spec/cuba_template_generator_spec.rb
+++ spec/cuba_template_generator_spec.rb
@@ -17,6 +17,11 @@ describe 'create a api', Cuba::Generator do
     expect(File).to receive(:open).with('./api/api.rb', 'w+')
     subject_api.create_cuba_file
   end
+
+  it 'create_gemfile' do
+    expect(File).to receive(:open).with('./api/Gemfile', 'w+')
+    subject_api.create_gemfile
+  end
 end
 
 describe 'create a app', Cuba::Generator do
@@ -36,6 +41,11 @@ describe 'create a app', Cuba::Generator do
     expect(File).to receive(:open).with('./app/app.rb', 'w+')
     subject_app.create_cuba_file
   end
+
+  it 'create_gemfile' do
+    expect(File).to receive(:open).with('./app/Gemfile', 'w+')
+    subject_app.create_gemfile
+  end
 end
 
 describe 'create a blog', Cuba::Generator do
@@ -54,5 +64,10 @@ describe 'create a blog', Cuba::Generator do
   it 'create_cuba_file' do
     expect(File).to receive(:open).with('./blog/blog.rb', 'w+')
     subject_blog.create_cuba_file
+  end
+
+  it 'create_gemfile' do
+    expect(File).to receive(:open).with('./blog/Gemfile', 'w+')
+    subject_blog.create_gemfile
   end
 end
